15194ae9623d332a1e9d3dbc1397a57d387af285
Charlie Curtsinger <charlie@cs.umass.edu>
2012-10-10 14:32:34 -0400

    Config fixes

--- run.py
+++ run.py
@@ -4,29 +4,39 @@ import os
 import sys
 
 benchmarks = ['astar', 'bwaves', 'bzip2', 'calculix', 'gcc', 'gobmk', 'gromacs', 'h264ref', 'hmmer', 'lbm', 'leslie3d', 'libquantum', 'mcf', 'milc', 'namd', 'perlbench', 'sjeng', 'sphinx3', 'wrf', 'zeusmp']
-train_benchmarks = ['calculix', 'gcc', 'libquantum', 'mcf', 'mic', 'namd', 'sphinx3']
+train_benchmarks = ['calculix', 'gcc', 'lbm', 'libquantum', 'mcf', 'milc', 'namd', 'sphinx3', 'perlbench']
 
 iterations = 10
 to_run = []
 dont_run = []
+configs = ['code', 'code.stack', 'code.heap.stack', 'link']
+tune = 'base'
+run_configs = []
 
 for arg in sys.argv[1:]:
 	if arg in benchmarks:
 		to_run.append(arg)
 	elif arg.startswith('-') and arg[1:] in benchmarks:
 		dont_run.append(arg[1:])
+	elif arg in configs:
+		run_configs.append(arg)
+	elif arg == 'peak':
+		tune = 'peak'
 	else:
 		iterations = int(arg)
 
 if len(to_run) == 0:
 	to_run = benchmarks
 
+if len(run_configs) == 0:
+	run_configs = configs
+
 for bmk in dont_run:
 	if bmk in to_run:
 		to_run.remove(bmk)
 
-def runspec(bench, size, ext, n, rebuild=False):
-	cmd = 'runspec --config=szc --mach=linux --action=run --tune=base --size='+size+' --ext='+ext+' -n '+str(n)
+def runspec(bench, size, tune, ext, n, rebuild=True):
+	cmd = 'runspec --config=szc --mach=linux --action=run --tune='+tune+' --size='+size+' --ext='+ext+' -n '+str(n)
 	if rebuild:
 		cmd += ' --rebuild'
 	cmd += ' '+bench
@@ -39,9 +49,16 @@ for bmk in to_run:
 	else:
 		size = 'test'
 
-	runspec(bmk, size, 'code', iterations)
-	runspec(bmk, size, 'code.stack', iterations)
-	runspec(bmk, size, 'code.heap.stack', iterations)
-	for i in range(0, iterations):
-		runspec(bmk, size, 'link', 1, rebuild=True)
+	if 'code' in run_configs:
+		runspec(bmk, size, tune, 'code', iterations)
+	
+	if 'code.stack' in run_configs:
+		runspec(bmk, size, tune, 'code.stack', iterations)
+	
+	if 'code.heap.stack' in run_configs:
+		runspec(bmk, size, tune, 'code.heap.stack', iterations)
+
+	if 'link' in run_configs:
+		for i in range(0, iterations):
+			runspec(bmk, size, tune, 'link', 1, rebuild=True)
 
